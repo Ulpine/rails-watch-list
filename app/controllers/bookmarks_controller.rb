538c6a778932a6371c490360c0c7d3e2d2e3df46
LÃeo TREMOUREUX <ltremoureux@hotmail.fr>
2024-11-14 18:13:49 +0100

    movies and list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    # @movie = Movie.find(params[:movie_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    # @movie = Movie.find(params[:movie_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    # raise
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      @bookmarks = @list.bookmarks
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-
-
+  # has_and_belongs_to_many :movies
 
   validates :comment, length: { minimum: 6 }
   validates :list_id, uniqueness: { scope: :movie_id }
